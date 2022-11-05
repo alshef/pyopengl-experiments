7c0232ad7252caf9ec9ce0a676e3b4122419719a
Alexander <shevelev@chu.do>
2022-11-05 15:27:05 +0300

    implement triangle class

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame as pg
 from OpenGL.GL import *
+import numpy as np
+import ctypes
 
 
 class App:
@@ -26,6 +28,33 @@ class App:
 
     def quit(self):
         pg.quit()
+
+
+class Triangle:
+    def __init__(self) -> None:
+        # x, y, z, r, g, b
+        self.vertices = (
+            -0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
+            0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
+            0.0, 0.5, 0.0, 0.0, 0.0, 1.0                
+        )
+
+        self.vertices = np.array(self.vertices, dtype=np.float32)
+        self.vertex_count = 3
+        self.vao = glGenVertexArrays(1)
+        glBindVertexArray(self.vao)
+        self.vbo = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
+        glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
+
+        glEnableVertexAttribArray(0)
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
+        glEnableVertexAttribArray(1)
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
+
+    def destroy(self):
+        glDeleteVertexArrays(1, (self.vao,))
+        glDeleteBuffers(1, (self.vbo,))
 
 
 if __name__ == "__main__":
